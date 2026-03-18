b23c5983d45e3dfb1557f2ccb8e73b6fca88d416
Nandinihedaoo76 <nandinihedaoo30@gmail.com>
2026-03-18 18:12:29 +0530

    added

---
+++ ec2.tf
@@ -0,0 +1,15 @@
+provider "aws" {
+        region = eu-north-1
+}
+
+resource "aws_instance" "infra" {
+        ami = "ami-080254318c2d8932f"
+        instance_type = "t3.micro"
+        key_name = "dev123"
+        tags = {
+                Name = "infra"
+                env = "dev"
+        }
+        vpc_security_group_ids = ["sg-0301c5e1980e14581"]
+  
+}
